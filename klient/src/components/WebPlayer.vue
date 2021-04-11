59a1e0d190f9ea2e37585b8c270bd7266a801630
Iwo Strzeboński <iwo.strzebonski@gmail.com>
2021-04-11 19:28:33 +0200

    Poprawiono pasek postępu utworu

--- klient/src/components/WebPlayer.vue
+++ klient/src/components/WebPlayer.vue
@@ -7,6 +7,7 @@
           <img class="controls" src="http://localhost:3000/prevSong.svg" />
         </button>
       </div>
+
       <div id="play">
         <button v-if="!playing" @click="play">
           <img class="controls" src="http://localhost:3000/play.svg" />
@@ -16,6 +17,7 @@
           <img class="controls" src="http://localhost:3000/pause.svg" />
         </button>
       </div>
+
       <div id="next">
         <button @click="nextSong">
           <img
@@ -25,6 +27,10 @@
           />
         </button>
       </div>
+
+      <div id="input-container">
+        <input id="input" class="slider" value="0" step="1" type="range" @change="changeInput">
+      </div>
     </div>
   </div>
 </template>
@@ -32,42 +38,41 @@
 <script>
 export default {
   name: "WebPlayer",
-  props: {
-    msg: String,
-  },
-  data() {
-    return {
-      playing: false,
-    };
-  },
+  props: { msg: String, },
+  data() { return { playing: false, } },
   methods: {
-    play: async function (event) {
-      this.playing = !this.playing;
-      console.log("playing");
+    play: async function() {
+      this.playing = !this.playing
+      console.log("playing")
 
-      document.getElementById("audio").play();
+      document.getElementById("audio").play()
 
       while (this.playing) {
-        await new Promise((r) => window.setTimeout(r, 10));
+        await new Promise((r) => window.setTimeout(r, 10))
         document.getElementById("input").value = parseInt(
-          document.getElementById("audio").currentTime
-        );
+          document.getElementById("audio").currentTime)
+
+          if (document.getElementById("audio").currentTime === document.getElementById('audio').duration) {
+            document.getElementById('input').value = 0
+            this.playing = !this.playing
+            console.log("stopped")
+          }
       }
     },
-    pause: function (event) {
-      this.playing = !this.playing;
-      console.log("paused");
+    pause: function() {
+      this.playing = !this.playing
+      console.log("paused")
 
-      document.getElementById("audio").pause();
+      document.getElementById("audio").pause()
     },
-    nextSong: function (event) {
-      console.log("nextSong");
-    },
-    prevSong: function (event) {
-      console.log("prevSong");
-    },
+    nextSong: function() { console.log("nextSong") },
+    prevSong: function() { console.log("prevSong") },
+    changeInput: function() {
+      document.getElementById('audio').currentTime = document.getElementById('input').value
+    }
   },
-};
+}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
