b98bd0e921a2d31b37468d672fba3cee85bc248c
Iwo Strzeboński <iwo.strzebonski@gmail.com>
2021-04-13 17:10:30 +0200

    Poprawka formatowania w server.js

--- serwer/server.js
+++ serwer/server.js
@@ -51,7 +51,7 @@ function fileUpload(fields, files) {
             newPath = dir + 'cover.jpg'
         }
 
-        fs.rename(oldPath, newPath, function (err) {
+        fs.rename(oldPath, newPath, function(err) {
             if (err) return
         })
     }
@@ -66,7 +66,7 @@ function formHandler(req, res) {
 
     form.uploadDir = PATH + '/static/mp3/temp/'
 
-    form.parse(req, function (err, fields, files) {
+    form.parse(req, function(err, fields, files) {
         if (err) {
             return
         }
@@ -89,121 +89,120 @@ function redirect(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*')
 
     switch (req.method) {
-        case 'GET':
-            switch (req.url.substr(req.url.lastIndexOf('.') + 1)) {
-                case 'mp3':
-                    fs.readFile(PATH + '/static/mp3/' + decodeURI(req.url), function (err, data) {
-                        if (err) { return }
-                        let stats = fs.statSync(PATH + '/static/mp3/' + decodeURI(req.url))
-                        res.writeHead(200, {
-                            'Content-type': 'audio/mpeg',
-                            'Content-Length': stats.size,
-                            'Accept-Ranges': 'bytes'
-                        })
-                        res.write(data)
-                        res.end()
-                    })
-                    break
-
-                case 'svg':
-                    fs.readFile(PATH + '/static/img/' + decodeURI(req.url), function (err, data) {
-                        if (err) { return }
-                        res.writeHead(200, { 'Content-type': 'image/svg+xml' })
-                        res.write(data)
-                        res.end()
-                    })
-                    break
-
-                case 'jpg':
-                    fs.readFile(PATH + '/static/mp3/' + decodeURI(req.url).replace('.jpg', '') + '/cover.jpg', function (err, data) {
-                        if (err) {
-                            fs.readFile(PATH + '/static/img/default.jpg', function (err, defaultData) {
-                                if (err) { return }
-
-                                res.writeHead(200, { 'Content-type': 'image/jpeg' })
-                                res.write(defaultData)
-                                res.end()
-                            })
-                        } else {
-                            res.writeHead(200, { 'Content-type': 'image/jpeg' })
-                            res.write(data)
-                            res.end()
-                        }
-                    })
-                    break
-
-                case 'png':
-                    fs.readFile(PATH + '/static/img/' + decodeURI(req.url), function (err, data) {
-                        if (err) { return }
-                        res.writeHead(200, { 'Content-type': 'image/png' })
-                        res.write(data)
-                        res.end()
-                    })
-                    break
-
-                case 'js':
-                    fs.readFile(PATH + '/static/js/' + decodeURI(req.url), function (err, data) {
-                        if (err) { return }
-                        res.writeHead(200, { 'Content-type': 'text/javascript; charset=utf-8' })
-                        res.write(data)
-                        res.end()
-                    })
-                    break
-
-                case 'css':
-                    fs.readFile(PATH + '/static/css/' + decodeURI(req.url), function (err, data) {
-                        if (err) { return }
-                        res.writeHead(200, { 'Content-type': 'text/css; charset=utf-8' })
-                        res.write(data)
-                        res.end()
-                    })
-                    break
-
-                case 'ico':
-                    fs.readFile(PATH + '/favicon.ico', function (err, data) {
-                        if (err) { return }
-                        res.writeHead(200, { 'Content-type': 'image/vnd.microsoft.icon' })
-                        res.write(data)
-                        res.end()
-                    })
-                    break
-
-                default:
-                    if (req.url === '/admin' || req.url === '/admin.html') {
-                        fs.readFile(PATH + '/admin.html', function (err, data) {
-                            if (err) { return }
-                            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-                            res.write(data, 'utf-8')
-                            res.end()
-                        })
-                    }
-                    break
-            }
-
+    case 'GET':
+        switch (req.url.substr(req.url.lastIndexOf('.') + 1)) {
+        case 'mp3':
+            fs.readFile(PATH + '/static/mp3/' + decodeURI(req.url), function(err, data) {
+                if (err) { return }
+                let stats = fs.statSync(PATH + '/static/mp3/' + decodeURI(req.url))
+                res.writeHead(200, {
+                    'Content-type': 'audio/mpeg',
+                    'Content-Length': stats.size,
+                    'Accept-Ranges': 'bytes'
+                })
+                res.write(data)
+                res.end()
+            })
             break
 
-        case 'POST':
-            formHandler(req, res)
-            res.writeHead(200)
-            res.end()
+        case 'svg':
+            fs.readFile(PATH + '/static/img/' + decodeURI(req.url), function(err, data) {
+                if (err) { return }
+                res.writeHead(200, { 'Content-type': 'image/svg+xml' })
+                res.write(data)
+                res.end()
+            })
+            break
+
+        case 'jpg':
+            fs.readFile(PATH + '/static/mp3/' + decodeURI(req.url).replace('.jpg', '') + '/cover.jpg', function(err, data) {
+                if (err) {
+                    fs.readFile(PATH + '/static/img/default.jpg', function(err, defaultData) {
+                        if (err) { return }
+
+                        res.writeHead(200, { 'Content-type': 'image/jpeg' })
+                        res.write(defaultData)
+                        res.end()
+                    })
+                } else {
+                    res.writeHead(200, { 'Content-type': 'image/jpeg' })
+                    res.write(data)
+                    res.end()
+                }
+            })
+            break
+
+        case 'png':
+            fs.readFile(PATH + '/static/img/' + decodeURI(req.url), function(err, data) {
+                if (err) { return }
+                res.writeHead(200, { 'Content-type': 'image/png' })
+                res.write(data)
+                res.end()
+            })
+            break
+
+        case 'js':
+            fs.readFile(PATH + '/static/js/' + decodeURI(req.url), function(err, data) {
+                if (err) { return }
+                res.writeHead(200, { 'Content-type': 'text/javascript; charset=utf-8' })
+                res.write(data)
+                res.end()
+            })
+            break
+
+        case 'css':
+            fs.readFile(PATH + '/static/css/' + decodeURI(req.url), function(err, data) {
+                if (err) { return }
+                res.writeHead(200, { 'Content-type': 'text/css; charset=utf-8' })
+                res.write(data)
+                res.end()
+            })
+            break
+
+        case 'ico':
+            fs.readFile(PATH + '/favicon.ico', function(err, data) {
+                if (err) { return }
+                res.writeHead(200, { 'Content-type': 'image/vnd.microsoft.icon' })
+                res.write(data)
+                res.end()
+            })
+            break
+
+        default:
+            if (req.url === '/admin' || req.url === '/admin.html') {
+                fs.readFile(PATH + '/admin.html', function(err, data) {
+                    if (err) { return }
+                    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+                    res.write(data, 'utf-8')
+                    res.end()
+                })
+            }
+            break
+        }
+
+        break
 
     case 'POST':
         formHandler(req, res)
+        res.writeHead(200)
+        res.end()
 
-        case 'OPTIONS':
-            res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-            res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-            res.end()
+        break
 
-            break
+    case 'OPTIONS':
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+        res.end()
+
+        break
     }
 }
 
-const server = http.createServer(function (req, res) {
+const server = http.createServer(function(req, res) {
     redirect(req, res)
 })
 
-server.listen(PORT, function () {
+server.listen(PORT, function() {
     // eslint-disable-next-line no-console
     console.log('Server started on port:', PORT)
 })
